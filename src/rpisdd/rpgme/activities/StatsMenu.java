e17f1cb12fa3bdcf77e9e405841a279ad0f62abd
Drew Wright <PandaCodeMaster@gmail.com>
2013-10-16 18:25:38 -0400

    Added more stats to stat page. Display them via text on the stat menu

--- src/rpisdd/rpgme/activities/StatsMenu.java
+++ src/rpisdd/rpgme/activities/StatsMenu.java
@@ -1,20 +1,76 @@
 package rpisdd.rpgme.activities;
 
 import rpisdd.rpgme.R;
+import rpisdd.rpgme.gamelogic.player.Player;
+import rpisdd.rpgme.gamelogic.player.Stats;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 public class StatsMenu extends Fragment {
 	
 	public StatsMenu(){}
 	
+	TextView strengthWidget;
+	TextView intWidget;
+	TextView willWidget;
+	TextView spiritWidget;
+	
+	TextView nameWidget;
+	TextView levelWidget;
+	TextView toNextWidget;
+	TextView totalExpWidget;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
+    	View v = inflater.inflate(R.layout.stats_menu, container, false);
+    	
+    	Stats playerStats = Player.getPlayer(this).stats;
+    	
+    	//get stats from player
+    	int currentStr = playerStats.baseStrength;
+    	int currentInt = playerStats.baseIntelligence;
+    	int currentWill = playerStats.baseWill;
+    	int currentSpr = playerStats.baseSpirit;
+    	
+    	int level = playerStats.level;
+    	int toNext = playerStats.toNextExp;
+    	int total = playerStats.level;
+    	
+    	String playerName = "Kevin";
+    		
+    	//display the stats
+    	strengthWidget = (TextView) v.findViewById(R.id.strengthDisplay);
+    	strengthWidget.setText("Strength: " + currentStr);
+    	
+    	intWidget = (TextView) v.findViewById(R.id.intDisplay);
+    	intWidget.setText("Int: " + currentInt);
+    	
+    	willWidget = (TextView) v.findViewById(R.id.willDisplay);
+    	willWidget.setText("Will: " + currentWill);
+    	
+    	spiritWidget = (TextView) v.findViewById(R.id.spiritDisplay);
+    	spiritWidget.setText("Spirit: " + currentSpr); 	
+    	
+    	nameWidget = (TextView) v.findViewById(R.id.nameDisplay);
+    	nameWidget.setText(playerName); 
+    	
+    	levelWidget = (TextView) v.findViewById(R.id.levelDisplay);
+    	levelWidget.setText("Lv: " + level); 
+    	
+    	toNextWidget = (TextView) v.findViewById(R.id.nextExpDisplay);
+    	toNextWidget.setText("To Next: " + toNext); 
+    	
+    	totalExpWidget = (TextView) v.findViewById(R.id.totalExpDisplay);
+    	totalExpWidget.setText("Total exp: " + total); 
+    	
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.stats_menu, container, false);
+    	return v;
     }
 }
--- src/rpisdd/rpgme/gamelogic/player/Stats.java
+++ src/rpisdd/rpgme/gamelogic/player/Stats.java
@@ -2,17 +2,25 @@ package rpisdd.rpgme.gamelogic.player;
 
 public class Stats 
 {
+	//basic player info
+	public String playerName;
+	
+	//levels and experience
+	public int level;
+	public int toNextExp;
+	public int totalExp;
+	
 	//The player's base stats. To be displayed in "stats" menu
-	int baseStrength;
-	int baseIntelligence;
-	int baseWill;
-	int baseSpirit;
+	public int baseStrength;
+	public int baseIntelligence;
+	public int baseWill;
+	public int baseSpirit;
 	
 	//The player's modified stats by the weapon he is carrying. 
-	int netStrength;
-	int netIntelligence;
-	int netWill;
-	int netSpirit;
+	public int netStrength;
+	public int netIntelligence;
+	public int netWill;
+	public int netSpirit;
 	
 	public Stats(){}
 	
@@ -21,6 +29,23 @@ public class Stats
 		baseIntelligence = aIntel;
 		baseWill = aWill;
 		baseSpirit = aSpirit;
+	}
+	
+	public Stats(String aName, int aLevel, int aToNext, int aTotal, int aStrength,int aIntel,int aWill,int aSpirit){
+		playerName = aName;
+		level = aLevel;
+		toNextExp = aToNext;
+		totalExp = aTotal;
+		
+		baseStrength = aStrength;
+		baseIntelligence = aIntel;
+		baseWill = aWill;
+		baseSpirit = aSpirit;
+	}
+	
+	public int getBaseStr()
+	{
+		return baseStrength;
 	}
 	
 	
