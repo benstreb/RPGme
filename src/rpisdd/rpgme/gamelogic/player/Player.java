3b44f5697feb766c9a0f12909436df14ee5d9f8b
Benjamin Streb <Bstreb@gmail.com>
2013-10-22 20:31:32 +0000

    Formatting.
    The amazing eclipse auto-formatter.

--- src/rpisdd/rpgme/gamelogic/player/Player.java
+++ src/rpisdd/rpgme/gamelogic/player/Player.java
@@ -1,29 +1,29 @@
 package rpisdd.rpgme.gamelogic.player;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.support.v4.app.Fragment;
 import rpisdd.rpgme.R;
-import rpisdd.rpgme.activities.MainActivity;
 import rpisdd.rpgme.gamelogic.items.Inventory;
 import rpisdd.rpgme.gamelogic.quests.QuestManager;
 
 public class Player {
 	final static int EXP_PER_LEVEL = 100;
-	
+
 	private static Player player = null;
 	String name;
 	String classs;
 	int avatarId;
-	
+
 	public QuestManager questManager;
 	public Stats stats;
 	public Inventory inventory;
-	
+
 	private int gold;
 	private int energy;
-	
+
 	public Player(CharSequence name, CharSequence classs, int avatarId) {
 		this.name = name.toString();
 		this.classs = classs.toString();
@@ -34,200 +34,187 @@ public class Player {
 		this.gold = 100;
 		this.energy = 10;
 	}
-	
-	public String getName(){ return name; }
-	public String getClasss(){ return classs; }
-	
-	public int getGold(){ return gold; }
-	
-	public void addGold(int amount){
+
+	public String getName() {
+		return name;
+	}
+
+	public String getClasss() {
+		return classs;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public void addGold(int amount) {
 		gold += amount;
 	}
-	
-	public void deductGold(int amount){
+
+	public void deductGold(int amount) {
 		gold -= amount;
-		if(gold < 0){
+		if (gold < 0) {
 			gold = 0;
 		}
 	}
-	
+
 	/*
-		returns the player's current energy
-	*/
-	public int getEnergy()
-	{
+	 * returns the player's current energy
+	 */
+	public int getEnergy() {
 		return stats.baseEnergy;
 	}
-	
+
 	/*
 	 * Increases the player's current energy
 	 */
-	public void addEnergy(int amount)
-	{
+	public void addEnergy(int amount) {
 		energy += amount;
-		if(energy > getMaxEnergy())
-		{
+		if (energy > getMaxEnergy()) {
 			energy = getMaxEnergy();
 		}
 	}
-	
+
 	/*
 	 * Decreases the player's current energy
 	 */
-	public void deductEnergy(int amount)
-	{
+	public void deductEnergy(int amount) {
 		energy -= amount;
-		if(energy < 0)
-		{
+		if (energy < 0) {
 			energy = 0;
 		}
 	}
-	
+
 	/*
 	 * Returns exp to next level up
 	 */
-	public int getExpForLevel(int aLevel)
-	{
+	public int getExpForLevel(int aLevel) {
 		return EXP_PER_LEVEL * aLevel;
 	}
-	
-	public int getNextExp()
-	{
+
+	public int getNextExp() {
 		return getExpForLevel(getLevel()) - getTotalExp();
 	}
-	
+
 	/*
 	 * Returns total exp accumulated.
 	 */
-	public int getTotalExp()
-	{
+	public int getTotalExp() {
 		return stats.totalExp;
 	}
-	
+
 	/*
 	 * Increases earned exp
 	 */
-	public void addExp(int amount)
-	{
+	public void addExp(int amount) {
 		stats.totalExp += amount;
-		if(stats.totalExp > getExpForLevel(getLevel()))
-		{
+		if (stats.totalExp > getExpForLevel(getLevel())) {
 			levelUp();
 		}
 	}
-	
+
 	/*
 	 * Returns the player's level
 	 */
-	public int getLevel()
-	{
+	public int getLevel() {
 		return stats.level;
 	}
-	
+
 	/*
 	 * Levels up the player
 	 */
-	public void levelUp()
-	{
+	public void levelUp() {
 		incMaxEnergy(5);
 		stats.level++;
 	}
-	
-	//Stat functions/////////////////////////////////////////////
-	
+
+	// Stat functions/////////////////////////////////////////////
+
 	/*
 	 * Returns player's max energy
 	 */
-	public int getMaxEnergy()
-	{
+	public int getMaxEnergy() {
 		return stats.baseEnergy;
 	}
-	
+
 	/*
 	 * Increases the player's max energy
 	 */
-	public void incMaxEnergy(int amount)
-	{
+	public void incMaxEnergy(int amount) {
 		stats.baseEnergy += amount;
 	}
-	
+
 	/*
 	 * Returns the player's strength
 	 */
-	public int getStrength()
-	{
+	public int getStrength() {
 		return stats.baseStrength;
 	}
-	
+
 	/*
 	 * Increases the player's strength
 	 */
-	public void incStrength(int amount)
-	{
+	public void incStrength(int amount) {
 		stats.baseStrength += amount;
 	}
-	
+
 	/*
 	 * Returns the player's intelligence
 	 */
-	public int getInt()
-	{
+	public int getInt() {
 		return stats.baseIntelligence;
 	}
-	
+
 	/*
 	 * Increases the player's intelligence
 	 */
-	public void incInt(int amount)
-	{
+	public void incInt(int amount) {
 		stats.baseIntelligence += amount;
 	}
-	
+
 	/*
 	 * Returns the player's will
 	 */
-	public int getWill()
-	{
+	public int getWill() {
 		return stats.baseWill;
 	}
-	
+
 	/*
 	 * Increases the player's will
 	 */
-	public void incWill(int amount)
-	{
+	public void incWill(int amount) {
 		stats.baseWill += amount;
 	}
-	
+
 	/*
 	 * Returns the player's spirit
 	 */
-	public int getSpirit()
-	{
+	public int getSpirit() {
 		return stats.baseSpirit;
 	}
-	
+
 	/*
 	 * Increases the player's spirit
 	 */
-	public void incSpirit(int amount)
-	{
+	public void incSpirit(int amount) {
 		stats.baseSpirit += amount;
 	}
-	
-	/////////////////////////////////////////////////
-	
+
+	// ///////////////////////////////////////////////
+
 	public static Player getPlayer() {
 		return player;
 	}
-	
-	//Todo: Eliminate this function, and replace all occurrences of getPlayer(this) with getPlayer()
-	public static Player getPlayer(Fragment fragment){
+
+	// Todo: Eliminate this function, and replace all occurrences of
+	// getPlayer(this) with getPlayer()
+	public static Player getPlayer(Fragment fragment) {
 		return player;
 	}
-	
+
 	public void savePlayer(Activity activity) {
 		questManager.saveQuestsToDatabase(activity);
+		inventory.saveItemsToDatabase(activity);
 		SharedPreferences p = activity.getPreferences(Context.MODE_PRIVATE);
 		Editor e = p.edit();
 		stats.save(e);
@@ -237,24 +224,23 @@ public class Player {
 		e.putBoolean("playerExists", true);
 		e.commit();
 	}
-	
+
 	public static void loadPlayer(Activity activity) {
 		SharedPreferences pref = activity.getPreferences(Context.MODE_PRIVATE);
 		assert pref.getBoolean("playerExists", false);
-		Player p = new Player(
-				pref.getString("name", "Missingno"),
-				pref.getString("class", "Bird, Water"),
-				pref.getInt("avatarId", R.drawable.splash_screen)
-				);
+		Player p = new Player(pref.getString("name", "Missingno"),
+				pref.getString("class", "Bird, Water"), pref.getInt("avatarId",
+						R.drawable.splash_screen));
 		p.questManager.loadQuestsFromDatabase(activity);
 		p.inventory.loadItemsFromDatabase(activity);
 		p.stats = Stats.load(pref);
 		player = p;
 	}
-	
-	public static void createPlayer(CharSequence name, CharSequence classs, int avatarId) {
+
+	public static void createPlayer(CharSequence name, CharSequence classs,
+			int avatarId) {
 		assert player == null;
 		player = new Player(name, classs, avatarId);
 	}
-	
+
 }
