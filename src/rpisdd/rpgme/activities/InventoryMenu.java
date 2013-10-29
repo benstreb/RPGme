3448141f13d4b8dfeb78b6860ed7ffbc60575187
Benjamin Streb <Bstreb@gmail.com>
2013-10-29 23:27:52 +0000

    Fixed some bugs with equipping.
    Fixed a null pointer exception when no item was selected, as well
    as a bug where the old weapon didn't go away.
    Also formatting. And now I understand why separating reformatting
    and code changes is considered a best practice.

--- src/rpisdd/rpgme/activities/InventoryMenu.java
+++ src/rpisdd/rpgme/activities/InventoryMenu.java
@@ -1,9 +1,6 @@
 package rpisdd.rpgme.activities;
 
-import java.io.File;
 import java.util.ArrayList;
-
-import com.squareup.picasso.Picasso;
 
 import rpisdd.rpgme.R;
 import rpisdd.rpgme.gamelogic.items.Item;
@@ -13,8 +10,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,6 +22,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class InventoryMenu extends ListFragment implements OnClickListener {
 
 	Button sell;
@@ -32,16 +31,17 @@ public class InventoryMenu extends ListFragment implements OnClickListener {
 	View selectedItem;
 	int selectedItemIndex;
 
-	public InventoryMenu() {}
+	public InventoryMenu() {
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		
+
 		selectedItem = null;
 		selectedItemIndex = -1;
-		
+
 		View v = inflater.inflate(R.layout.inventory_menu, container, false);
 
 		sell = (Button) v.findViewById(R.id.sellItem);
@@ -50,28 +50,28 @@ public class InventoryMenu extends ListFragment implements OnClickListener {
 		use = (Button) v.findViewById(R.id.useItem);
 		use.setOnClickListener(this);
 
-		
 		updateButtons();
-		
+
 		return v;
 	}
-	
-	//Will enable or disable buttons, depending on the situation.
-	public void updateButtons(){
-		Item i = Player.getPlayer().getInventory().getItems().get(selectedItemIndex);
-		if (selectedItem == null){
+
+	// Will enable or disable buttons, depending on the situation.
+	public void updateButtons() {
+		if (selectedItem == null) {
 			sell.setEnabled(false);
 			use.setEnabled(false);
 		} else {
+			Item i = Player.getPlayer().getInventory().getItems()
+					.get(selectedItemIndex);
 			sell.setEnabled(true);
-			if (i.isUsable()) {
+			if (i != null && i.isUsable()) {
 				use.setEnabled(true);
 			} else {
 				use.setEnabled(false);
 			}
 		}
 	}
-	
+
 	@Override
 	public void onResume() {
 		fillListView(getView());
@@ -80,137 +80,149 @@ public class InventoryMenu extends ListFragment implements OnClickListener {
 
 	// Take a list of items, and fill up the list view with those entries
 	public void fillListView(View v) {
-		
+
 		Player p = Player.getPlayer();
 		ItemAdapter adapter = new ItemAdapter(getActivity(),
-				R.layout.shop_item,p.getInventory().getItems());
-		
+				R.layout.shop_item, p.getInventory().getItems());
+
 		setListAdapter(adapter);
-		
+
 		selectedItem = null;
 		selectedItemIndex = -1;
-		
+
 		updateButtons();
 	}
-	
+
 	private class ItemAdapter extends ArrayAdapter<Item> {
 
 		ArrayList<Item> items;
-		
-        public ItemAdapter(Context context, int textViewResourceId, ArrayList<Item> items) {
-                super(context, textViewResourceId, items);
-                this.items = items;
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-                View v = convertView;
-                if(v == null){
-                    v = LayoutInflater.from(getActivity()).inflate(R.layout.inventory_item, null);
-                }
-                Item i = items.get(position);
-                if (i != null) {
-                    TextView name = (TextView) v.findViewById(R.id.inventoryItemName);
-                    name.setText(i.getName());  
-                    
-                    ImageView image = (ImageView) v.findViewById(R.id.inventoryItemImage);
-                    name.setText("Name: " + i.getName());     
-                    Picasso.with(getActivity())
-                    	.load(i.getImagePath())
-                    	.into(image);
-                }
-                return v;
-        }
+
+		public ItemAdapter(Context context, int textViewResourceId,
+				ArrayList<Item> items) {
+			super(context, textViewResourceId, items);
+			this.items = items;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if (v == null) {
+				v = LayoutInflater.from(getActivity()).inflate(
+						R.layout.inventory_item, null);
+			}
+			Item i = items.get(position);
+			if (i != null) {
+				TextView name = (TextView) v
+						.findViewById(R.id.inventoryItemName);
+				name.setText(i.getName());
+
+				ImageView image = (ImageView) v
+						.findViewById(R.id.inventoryItemImage);
+				name.setText("Name: " + i.getName());
+				Picasso.with(getActivity()).load(i.getImagePath()).into(image);
+			}
+			return v;
+		}
 	}
-	
+
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 
 		super.onListItemClick(l, v, position, id);
 
 		selectedItemIndex = position;
-		
-		if (selectedItem != null)
+
+		if (selectedItem != null) {
 			selectedItem.setBackgroundColor(Color.TRANSPARENT);
+		}
 
 		v.setBackgroundColor(Color.GRAY);
 
 		v.setSelected(true);
 		selectedItem = v;
-		
+
 		updateButtons();
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		
-		case R.id.useItem: {
-			
-			AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
-           builder1.setMessage("Use this item?");
-           builder1.setCancelable(true);
-           builder1.setPositiveButton("Use",
-                   new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   dialog.cancel();
-                   useItem();
-               }
-           });
-           
-           builder1.setNegativeButton("Don't Use",
-                   new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   dialog.cancel();
-               }
-           });
 
-           AlertDialog alert11 = builder1.create();
-           alert11.show();
-           
+		case R.id.useItem: {
+
+			AlertDialog.Builder builder1 = new AlertDialog.Builder(
+					getActivity());
+			builder1.setMessage("Use this item?");
+			builder1.setCancelable(true);
+			builder1.setPositiveButton("Use",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+							useItem();
+						}
+					});
+
+			builder1.setNegativeButton("Don't Use",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+						}
+					});
+
+			AlertDialog alert11 = builder1.create();
+			alert11.show();
+
 			break;
 		}
 		case R.id.sellItem: {
-			
-		   AlertDialog.Builder builder1 = new AlertDialog.Builder(getActivity());
-           builder1.setMessage("Sell this item?");
-           builder1.setCancelable(true);
-           builder1.setPositiveButton("Sell",
-           new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   dialog.cancel();
-                   sellItemToShop();
-               }
-           });
-           
-           builder1.setNegativeButton("Don't Sell",
-                   new DialogInterface.OnClickListener() {
-               public void onClick(DialogInterface dialog, int id) {
-                   dialog.cancel();
-               }
-           });
 
-           AlertDialog alert11 = builder1.create();
-           alert11.show();
-           
+			AlertDialog.Builder builder1 = new AlertDialog.Builder(
+					getActivity());
+			builder1.setMessage("Sell this item?");
+			builder1.setCancelable(true);
+			builder1.setPositiveButton("Sell",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+							sellItemToShop();
+						}
+					});
+
+			builder1.setNegativeButton("Don't Sell",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int id) {
+							dialog.cancel();
+						}
+					});
+
+			AlertDialog alert11 = builder1.create();
+			alert11.show();
+
 			break;
 		}
 		default:
 			break;
 		}
 	}
-	
-	public void sellItemToShop(){
+
+	public void sellItemToShop() {
 		Player p = Player.getPlayer();
-		p.addGold(p.getInventory().getItems().get(selectedItemIndex).getRefundPrice());
+		p.addGold(p.getInventory().getItems().get(selectedItemIndex)
+				.getRefundPrice());
 		p.getInventory().removeAt(selectedItemIndex);
 		fillListView(getView());
 	}
-	
-	public void useItem(){
+
+	public void useItem() {
 		Player p = Player.getPlayer();
-		p.getInventory().getItems().get(selectedItemIndex).useMe(p, selectedItemIndex);
-		
+		p.getInventory().getItems().get(selectedItemIndex)
+				.useMe(p, selectedItemIndex);
+		Log.i("items", String.format("Player has %s and %s equipped", p
+				.getInventory().getWeapon(), p.getInventory().getArmor()));
 		fillListView(getView());
 	}
-	
 }
