5b18bd1f8a34f2e3efaf7de1788b073d8e40ac85
racoonsmakeokayfriends <amanda.knight314@gmail.com>
2013-11-19 15:01:10 -0500

    updated the unconcious page

--- src/rpisdd/rpgme/activities/UnconsciousWarning.java
+++ src/rpisdd/rpgme/activities/UnconsciousWarning.java
@@ -1,11 +1,13 @@
 package rpisdd.rpgme.activities;
 
 import rpisdd.rpgme.R;
+import rpisdd.rpgme.gamelogic.player.Player;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class UnconsciousWarning extends Fragment {
 
@@ -20,6 +22,13 @@ public class UnconsciousWarning extends Fragment {
 		// energy
 		View v = inflater.inflate(R.layout.unconscious_splash_screen,
 				container, false);
+
+		ImageView avatarImage;
+		Player thePlayer = Player.getPlayer();
+		int avatarId = thePlayer.getAvatar();
+
+		avatarImage = (ImageView) v.findViewById(R.id.unconscious_av_img);
+		avatarImage.setImageResource(avatarId);
 		return v;
 	}
 }
