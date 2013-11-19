485a1d947c96eb9c2497eac3e972e9b13b407ee5
Drew Wright <PandaCodeMaster@gmail.com>
2013-11-19 16:53:43 -0500

    Updated combat equation. Damage done is now atk * (atk / def) with some normal distribution variance thrown in on the atk

--- src/rpisdd/rpgme/gamelogic/dungeon/model/Combat.java
+++ src/rpisdd/rpgme/gamelogic/dungeon/model/Combat.java
@@ -1,10 +1,14 @@
 package rpisdd.rpgme.gamelogic.dungeon.model;
 
+import java.util.Random;
+
 import rpisdd.rpgme.gamelogic.player.StatType;
 import android.util.Log;
 
 //A class to hold functions related to combat
 public class Combat {
+
+	public static double ATTACK_VARIANCE = 0.35;
 
 	// Represents an attack delivered by any source
 	// Holds the stattype associated with the attack
@@ -23,15 +27,34 @@ public class Combat {
 	}
 
 	// The equation to use for calculating damage
-	public static int CalculateAttackDamage(int atk, int def) {
-		Log.d("CombatDebug", "Combat: base damage: " + atk + "\n");
-		Log.d("CombatDebug", "Combat: defense: " + atk + "\n");
-		atk = atk - (atk * (def / (def + 100)));
+	public static int CalculateAttackDamage(int atk_, int def_) {
+		Log.d("CombatDebug", "Combat: base damage: " + atk_ + "\n");
+		Log.d("CombatDebug", "Combat: defense: " + def_ + "\n");
+
+		// add range of damage to make combat for interesting
+		double atk = atk_ + AttackVariance(atk_);
+		double def = def_;
+
+		// how attack and defense relate to determine damage
+		atk = atk * (atk / (atk + def));
 		if (atk < 1) {
-			Log.d("CombatDebug", "Combat: result post def: " + atk + "\n");
 			atk = 1;
 		}
-		return atk;
+		Log.d("CombatDebug", "Combat: result post def: " + atk + "\n");
+		return (int) (atk);
+	}
+
+	public static int AttackVariance(int atk_) {
+		Random gen = new Random();
+
+		// by dividing by 3, we force 99.7% of results
+		// to be within the variance range
+		double atk = atk_;
+		double effectiveVariance = ATTACK_VARIANCE / 3.0;
+
+		int variance = (int) (gen.nextGaussian() * atk * effectiveVariance);
+		Log.d("CombatDebug", "Variance: " + variance + "\n");
+		return variance;
 	}
 
 }
